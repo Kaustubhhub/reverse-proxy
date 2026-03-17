f7019570a9f2e3755f50b799c49c1789bcede366
kaustubh <kaustubhkumbhare02@gmail.com>
2026-03-17 08:12:51 +0000

    hardcoded backend server rp

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
 
 func main() {
-	fmt.Println("Reverse proxy init")
+
+	backend_servers := [...]string{"http://localhost:9001", "http://localhost:9002"}
+
+	target, _ := url.Parse()
+
+	proxy := httputil.NewSingleHostReverseProxy(target)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Forwarding request to backend server")
+		proxy.ServeHTTP(w, r)
+	})
+
+	log.Println("Proxy running on :8080")
+	http.ListenAndServe(":8080", nil)
 }
